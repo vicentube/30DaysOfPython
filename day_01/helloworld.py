853f59d2ba281ebe0260be98a33b715feb04621b
Vicente Ubeda <info@vicentube.com>
2023-03-29 11:01:18 -0400

    Day 1

---
+++ day_01/helloworld.py
@@ -0,0 +1,32 @@
+# Excercise 1
+
+import sys
+print(sys.version)
+
+# Excercise 2
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# Excercise 3
+
+print('John')
+print('Smith')
+print('USA')
+print('I am enjoying 30 days of python')
+
+# Excercise 4
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('John'))
+print(type('Smith'))
+print(type('USA'))
